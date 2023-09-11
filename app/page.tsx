151ebfdc93e7758752a9ab29c70bf6fe01904571
AlexDjangoX <alexmonk17@gmail.com>
2023-09-11 08:58:06 +0200

    removed client directive from layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 
-import { getProfile } from '@/sanity/sanity.query';
-
 type Props = {};
 
 const Home = async (props: Props) => {
-  const { profile } = await getProfile();
-
-  console.log(profile);
-
   return <div>Home</div>;
 };
 
