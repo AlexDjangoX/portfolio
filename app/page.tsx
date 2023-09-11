d4d1e54bbb14cd511d9c88435b11dab04652ee52
AlexDjangoX <alexmonk17@gmail.com>
2023-09-11 11:47:57 +0200

    imported Hero, Skills and Services into page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import Hero from '@/components/Hero';
+import Skills from '@/components/Skills';
+import Services from '@/components/Services';
 
 type Props = {};
 
-const Home = async (props: Props) => {
-  return <div>Home</div>;
+const Home = (props: Props) => {
+  return (
+    <>
+      <Hero />
+      <Skills />
+      <Services />
+    </>
+  );
 };
 
 export default Home;
