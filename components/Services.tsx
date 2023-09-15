94445962e6e6181c9487bc60f8a9702bcb05bbe4
AlexDjangoX <alexmonk17@gmail.com>
2023-09-15 12:43:26 +0200

    cleaned up CSS in Skills.jsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -16,7 +16,7 @@ const Services: React.FC = async () => {
 
   return (
     <section className="flex  flex-col items-center justify-center bg-white-800 px-[5.3125rem] py-[4.5rem] dark:bg-black-300">
-      <h2 className="mb-[2.5rem] flex flex-row flex-wrap text-[1.6rem] font-bold leading-leading4_0rem tracking-tracking_64 text-black-200 dark:bg-black-300 dark:text-white-900 vs:justify-center md:text-[3rem]">
+      <h2 className="leading-leading4_0rem tracking-tracking_64 mb-[2.5rem] flex flex-row flex-wrap text-[1.6rem] font-bold text-black-200 dark:bg-black-300 dark:text-white-900 vs:justify-center md:text-[3rem]">
         What
         <div className="mx-2">
           <span className="custom-underline z-10">service</span>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,14 +13,14 @@ const Skills: React.FC = async () => {
   const skills = await getSkills();
 
   return (
-    <section className="w-full bg-white-900 px-[5.3125rem] py-[4.5rem] dark:bg-black-200">
-      <h2 className="mb-[2.5rem] flex justify-center text-[2.25rem] font-bold leading-leading4_0rem tracking-tracking_64 text-black-200 dark:bg-black-200 dark:text-white-900 md:text-4xlplus">
+    <section className="w-full bg-white-900 px-[1.5rem] py-[3rem] dark:bg-black-200 sm:px-[5.3rem]">
+      <h2 className="mb-[2.5rem] flex justify-center text-[2.25rem] font-bold leading-[2.6rem] tracking-[-0.36px] text-black-200 dark:bg-black-200 dark:text-white-900 md:text-[3rem] md:leading-[3.45rem] md:tracking-[-0.48px]">
         <span className="custom-underline z-10">My Skills</span> <br />
       </h2>
-      <div className="flex flex-wrap justify-center gap-x-4 sm:gap-y-[2.5rem]">
+      <div className="flex flex-wrap justify-center gap-[2.5rem] last:gap-y-0 sm:gap-y-[2.5rem] md:gap-[3.4rem]">
         {skills?.map((skill: SkillsProps) => (
           <div key={skill?._id} className="mb-10">
-            <div className="relative flex h-[4rem] w-[4rem] items-center justify-center overflow-hidden rounded-full bg-white-800 transition-transform duration-150 hover:scale-125 dark:bg-black-300 sm:h-[6.25rem] sm:w-[6.25rem] md:mx-6">
+            <div className="relative flex h-[3.3rem] w-[3.3rem] items-center justify-center overflow-hidden rounded-full bg-white-800 transition-transform duration-150 hover:scale-125 dark:bg-black-300 md:mx-6 md:h-[6.25rem] md:w-[6.25rem]">
               <div className="relative h-12 w-12 overflow-hidden rounded-full">
                 <Image
                   src={skill?.image}
