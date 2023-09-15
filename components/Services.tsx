91dd52ed31c9b374d289f61eace1d109b291286e
AlexDjangoX <alexmonk17@gmail.com>
2023-09-15 13:19:59 +0200

    FIX: Service.tsx heading

--- components/Services.tsx
+++ components/Services.tsx
@@ -16,15 +16,16 @@ const Services: React.FC = async () => {
 
   return (
     <section className="flex  flex-col items-center justify-center bg-white-800 px-[5.3125rem] py-[4.5rem] dark:bg-black-300">
-      <h2 className="leading-leading4_0rem tracking-tracking_64 mb-[2.5rem] flex flex-row flex-wrap text-[1.6rem] font-bold text-black-200 dark:bg-black-300 dark:text-white-900 vs:justify-center md:text-[3rem]">
-        What
-        <div className="mx-2">
-          <span className="custom-underline z-10">service</span>
-        </div>
-        do I provide?
+      <h2 className="mb-[2.5rem]  text-[2.25rem] font-bold leading-[2.6rem] tracking-[-0.36px] text-black-200 dark:bg-black-200 dark:text-white-900 md:text-[3rem] md:leading-[3.45rem] md:tracking-[-0.48px]">
+        What{' '}
+        <div className="relative inline-flex">
+          <p className="z-20">service</p>
+          <div className="absolute z-10 mt-7 h-[0.8rem] w-full rounded-sm bg-secondary-dark md:mt-9 md:h-[1rem]" />
+        </div>{' '}
+        do I provide
       </h2>
 
-      <div className="flex flex-wrap justify-center gap-x-4 gap-y-[2.5rem]">
+      <div className="flex flex-wrap  justify-center gap-x-4 gap-y-[2.5rem] md:justify-around ">
         {services?.map((card: Service) => (
           <SkillsCard
             key={card?._id}
