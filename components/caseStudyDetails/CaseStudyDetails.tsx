11f80b9a2dc8f7b09bb8712d142d28be069a8aca
AlexDjangoX <alexmonk17@gmail.com>
2023-09-20 20:22:13 +0200

    removed unused file

--- components/caseStudyDetails/CaseStudyDetails.tsx
+++
@@ -1,9 +0,0 @@
-import { getCaseStudyDetails } from '@/sanity/sanity.query';
-
-const CaseStudyDetails = async () => {
-  const studyDetails = await getCaseStudyDetails();
-
-  return <div>CaseStudyDetails</div>;
-};
-
-export default CaseStudyDetails;
