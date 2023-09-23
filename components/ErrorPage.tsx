bb69416ae6af38880f67e8860ccc6c0a55c6dfe0
AlexDjangoX <alexmonk17@gmail.com>
2023-09-23 17:31:09 +0200

    styled error page

--- components/ErrorPage.tsx
+++ components/ErrorPage.tsx
@@ -1,20 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { errorImage } from '@/public/assets-png';
+
 const ErrorPage = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center px-10 py-8 pt-12">
-      <Image
-        src="https://i.ibb.co/ck1SGFJ/Group.png"
-        alt="Background Illustration"
-        width={400}
-        height={400}
-      />
-      <h1 className="my-2 text-2xl font-bold text-gray-800 dark:text-white-800">
+    <div className=" flex h-full w-full flex-col items-center justify-center bg-error-background px-10   pt-16">
+      <h1 className="my-2 pt-8 text-2xl font-bold text-gray-800 dark:text-gray-700">
         Looks like you have found the doorway to the great nothing
       </h1>
-
-      <p className="my-2 text-gray-800 dark:text-white-800">
+      <Image
+        src={errorImage}
+        alt="Error page Image - broken connection"
+        width={200}
+        height={200}
+      />
+      <p className="my-2 text-gray-800 dark:text-gray-700">
         Sorry about that! Please visit our homepage to get where you need to be.
       </p>
       <Link href="/" className="mt-4 flex justify-center">
@@ -22,13 +23,8 @@ const ErrorPage = () => {
           Homepage
         </button>
       </Link>
-      <div className="p-6">
-        <Image
-          src="https://i.ibb.co/G9DC8S0/404-2.png"
-          alt="Error Illustration"
-          width={200}
-          height={200}
-        />
+      <div className="p-3">
+        <p className="text-7xl dark:text-gray-700">404</p>
       </div>
     </div>
   );
