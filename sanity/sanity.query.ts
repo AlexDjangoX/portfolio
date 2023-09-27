59dbab99591323c863461d7cb3a20bebd7b6aebb
AlexDjangoX <alexmonk17@gmail.com>
2023-09-27 08:06:46 +0200

    added environmemtal variables on vercel - needed re-deploy

--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -1,7 +1,7 @@
 import { groq } from 'next-sanity';
 import client from './sanity.client';
 
-const revalidate = 1800;
+const revalidate = 1801;
 
 export async function getProfile() {
   return client.fetch(
