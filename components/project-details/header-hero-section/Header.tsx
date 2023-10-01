44ecba94e71eab5f1ead5a1c3bb7b28b09284224
AlexDjangoX <alexmonk17@gmail.com>
2023-10-01 19:06:23 +0200

    Header.tsx styles for project-details

--- components/project-details/header-hero-section/Header.tsx
+++ components/project-details/header-hero-section/Header.tsx
@@ -8,7 +8,7 @@ const Header = ({ headingUnderline, heading }: DetailsHeaderType) => (
       type="spring"
       delay={0.5}
       duration={2.75}
-      className="mb-[0.625rem] text-center font-semibold text-primary-light dark:text-primary-dark md:text-[1.25rem] md:leading-[1.95rem] md:tracking-[6px]"
+      className="mb-[1.625rem] text-center font-semibold text-primary-light dark:text-primary-dark xl:mb-[3.25rem] xl:text-[1.25rem] xl:leading-[1.95rem] xl:tracking-[6px]"
       tagType="h2"
     >
       WEB DEV PROJECT
@@ -18,12 +18,12 @@ const Header = ({ headingUnderline, heading }: DetailsHeaderType) => (
       type="spring"
       delay={0.5}
       duration={2.75}
-      className="text-center text-[2.625rem] font-bold leading-[3.45rem] tracking-[-0.026rem] text-black-200 dark:bg-black-300 dark:text-white-900 md:text-[4rem] md:leading-[1.95rem] md:tracking-[-0.64px]"
+      className="text-center text-[2.625rem] font-bold leading-[3.45rem] tracking-[-0.026rem] text-black-200 dark:bg-black-300 dark:text-white-900 xl:mb-[2.25rem] xl:text-[4rem] xl:leading-[1.95rem] xl:tracking-[-0.64px]"
       tagType="h1"
     >
       <div className="relative inline-flex ">
         <p className="z-20">{headingUnderline}</p>
-        <div className="absolute z-10 mt-7 h-[0.8rem] w-full rounded-sm bg-secondary-dark md:mt-14  md:h-[1rem]" />
+        <div className="absolute z-10 mt-9 h-[0.8rem] w-full rounded-sm bg-secondary-dark md:h-[1rem] xl:mt-7" />
       </div>
       {' - '} {heading}
     </MotionWrapper>
