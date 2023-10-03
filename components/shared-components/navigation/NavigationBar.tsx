f52c69dd69cc5ab17059ed306fe8c3bd5614a33a
AlexDjangoX <alexmonk17@gmail.com>
2023-10-03 12:04:14 +0200

    CSS navbar refactored

--- components/shared-components/navigation/NavigationBar.tsx
+++ components/shared-components/navigation/NavigationBar.tsx
@@ -28,25 +28,30 @@ const NavigationBar = () => {
         className="fixed inset-x-0 top-[4.1rem] z-50  h-0.5 origin-top-left bg-primary-light pt-1 dark:bg-primary-dark"
         style={{ scaleX }}
       />
-      <nav className="fixed z-40 flex h-16 w-full justify-between bg-white-800 py-[2.2rem] dark:bg-black-200">
-        <Link href="/" className="ml-[0.5rem] flex items-center md:ml-[4.4rem]">
-          <AlexanderBrand />
-        </Link>
-        <DesktopNavbar
-          showMobileNavbar={showMobileNavbar}
-          openImgSrc={openImgSrc}
-          pathname={pathname}
-          toggleMobile={toggleMobile}
-        />
-
-        {showMobileNavbar && (
-          <MobileNavbar
+      <nav className="fixed z-40  w-full  bg-white-800  dark:bg-black-200">
+        <div className="flex h-16 justify-between py-[2.2rem] xl:flex-row 2xl:mx-auto 2xl:max-w-[90rem]">
+          <Link
+            href="/"
+            className="ml-[0.5rem] flex items-center md:ml-[4.4rem]"
+          >
+            <AlexanderBrand />
+          </Link>
+          <DesktopNavbar
             showMobileNavbar={showMobileNavbar}
-            closeImgSrc={closeImgSrc}
+            openImgSrc={openImgSrc}
             pathname={pathname}
             toggleMobile={toggleMobile}
           />
-        )}
+
+          {showMobileNavbar && (
+            <MobileNavbar
+              showMobileNavbar={showMobileNavbar}
+              closeImgSrc={closeImgSrc}
+              pathname={pathname}
+              toggleMobile={toggleMobile}
+            />
+          )}
+        </div>
       </nav>
     </>
   );
